548a2561a72436be2e74c1b802004bcd47d82f62
ivan <ivpetr2003@gmail.com>
2019-08-08 19:02:46 +0300

    программа для управления динамикселями

--- firmware/Control_serial_noROS/Control_serial_noROS.ino
+++ firmware/Control_serial_noROS/Control_serial_noROS.ino
@@ -1,4 +1,14 @@
 #include <DynamixelWorkbench.h>
+/*Z
+   +----------------->Y
+   |
+   |
+   |
+   |
+   |
+   V
+   X
+ */
 
 // ОСЬ Z /////
 #define ZDYN1_ID 1
@@ -20,10 +30,18 @@
 #define PUMP_PIN 10
 
 // КООРДИНАТЫ
-float nextX = 0.0; //значение следующей координаты X
-float nextY = 0.0; //значение следующей координаты Y
-float lastX = 0.0; //значение предыдущей координаты X
-float lastY = 0.0; //значение предыдущей координаты Y
+#define cell_Xlenght 100 //длина 1-ой ячейки по оси X в милиметрах
+#define cell_Ylenght 100 //длина 1-ой ячейки по оси Y в милиметрах
+#define cell_Zlenght 100 //длина 1-ой ячейки по оси Z в милиметрах
+#define XLenght 7400 //длина рабочей части оси X в милиметрах
+#define YLenght 7400 //длина рабочей части оси Y в милиметрах
+#define ZLenght 7400 //длина рабочей части оси Z в милиметрах
+float nextX = 0.0;  //значение следующей координаты X
+float nextY = 0.0;  //значение следующей координаты Y
+float nextZ = 0.0;  //занчение следующей координаты Z
+float lastX = 0.0;  //значение предыдущей координаты X
+float lastY = 0.0;  //значение предыдущей координаты Y
+float lastZ = 0.0;   //значение предыдущей координаты Z
 
 // ЭНКОДЕРЫ
 #define ZDYN1_ENC_PIN 0 // пин прерывания энкодера 1 динамикселя оси Z
@@ -36,18 +54,109 @@ float lastY = 0.0; //значение предыдущей координаты 
 
 #define YDYN_ENC_PIN  6 // пин прерывания энкодера динамикселя оси Y
 
-int ZDYN1_ENC_VAL = 0;
-int ZDYN2_ENC_VAL = 0;
-int ZDYN3_ENC_VAL = 0;
-int ZDYN4_ENC_VAL = 0;
+#define wheel_ticks 65 // количество тиков колеса энкодера на оборот
+#define wheel_deametr  30 // диаметр колеса в милиметрах
+float wheel_round_L = 0.0; 
 
-DynamixelWorkbench Dxl;
+int ZDYN1_ENC_VAL = 0; //переменная счетчика 1 энкодера оси Z
+int ZDYN2_ENC_VAL = 0; //переменная счетчика 2 энкодера оси Z
+int ZDYN3_ENC_VAL = 0; //переменная счетчика 3 энкодера оси Z
+int ZDYN4_ENC_VAL = 0; //переменная счетчика 4 энкодера оси Z
+
+int XDYN1_ENC_VAL = 0; //переменная счетчика 1 энкодера оси X
+int XDYN2_ENC_VAL = 0; //переменная счетчика 2 энкодера оси X
+
+int YDYN_ENC_VAL = 0; //переменная счетчика энкодера оси Y
+
+DynamixelWorkbench Dxl; //инициализируем шину DXL
 
 void setup()
 {
+    wheel_round();
 
 }
+//функции прерываний энкодеров оси Z
+void ZDYN1_ENC_COUNTER(){
+    ZDYN1_ENC_VAL++; //первый энкодер Z
+}
+void ZDYN2_ENC_COUNTER(){
+    ZDYN2_ENC_VAL++; //второй энкодер оси Z
+}
+void ZDYN3_ENC_COUNTER(){
+    ZDYN3_ENC_VAL++; //третий энкодра оси Z
+}
+void ZDYN4_ENC_COUNTER(){
+    ZDYN4_ENC_VAL++; //четвертый энкодер оси Z
+}
+//функции прерываний экодеров оси X
+void XDYN1_ENC_COUNTER(){
+    XDYN1_ENC_VAL++; 
+}
+void XDYN2_ENC_COUNTER(){
+    XDYN2_ENC_VAL++;
+}
+//функции прерываний энкодеров оси Y
+void YDYN_ENC_COUNTER(){
+    YDYN_ENC_VAL++;
+}
+void wheel_round(){
+    wheel_round_L = PI * wheel_deametr;
+}
+void coordinates_to_ticks_and_goto(int nextX, int nextY){
+    int dX = 0; // дельта X
+    int dY = 0; // дельта Y
+    int dZ = 0; // дельта Z
+    float dlX = 0.0; // длина дельта X в милиметрах
+    float dlY = 0.0; // длина дельта Y в милиметрах
+    float dlZ = 0.0; // длина дельта Z в милиметрах
+    float obX = 0.0; // количество оборотов по оси Z
+    float obY = 0.0; // количество оборотов по оси Y
+    float obZ = 0.0; // количество оборотов по оси Z
+    double tiksX = 0;
+    double tiksY = 0;
+    double tiksZ = 0;
 
+    dX = max(nextX,lastX) - min(nextX,lastX); //расчет дельта X
+    dY = max(nextY,lastY) - min(nextY,lastY); //расчет дельта Y
+    dZ = max(nextZ,lastZ) - min(nextZ,lastZ); //расчет дельта Z
+
+    lastX = nextX;
+    lastY = nextY;
+    lastZ = nextZ;
+    dlX = dX * cell_Xlenght; // расчет длины дельта X в милиметрах
+    dlY = dY * cell_Ylenght; // расчет длины дельта Y в милиметрах
+    dlZ = dZ * cell_Zlenght; // расчет длины дельта Z в милиметрах 
+
+    obX = dlX / wheel_round_L;
+    obY = dlY / wheel_round_L;
+    obZ = dlZ / wheel_round_L;
+
+    tiksX = obX * wheel_ticks;
+    tiksY = obY * wheel_ticks;
+    tiksZ = obZ * wheel_ticks;
+}
+void go_to_coorinates(double tiksX, double tiksY, double tiksZ, int speed){
+    bool x_dir_rev = false;
+    bool y_dir_rev = false;
+    bool z_dir_rev = false;
+    if (nextX > lastY)
+        x_dir_rev = false;
+    else 
+        x_dir_rev = true;
+
+    if (nextY > lastY)
+        y_dir_rev = false;
+    else 
+        y_dir_rev = true;
+
+    if (nextZ > lastZ)
+        z_dir_rev = false;
+    else 
+        z_dir_rev = true;
+    
+    
+
+}
 void loop()
 {
 

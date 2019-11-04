aee6f657be4f9c0ec9a0489abe90ec2c22d65bab
Ivan <ivpetr2003@gmail.com>
2019-11-04 23:32:07 +0300

    изменена программа под arduino mega
    программа не работает

--- firmware/Kvantoriada_MEGA/Libs/Axis/Axis.cpp
+++ firmware/Kvantoriada_MEGA/Libs/Axis/Axis.cpp
@@ -1,32 +1,17 @@
-#include "Axis.h"
 #include <Arduino.h>
+#include "Axis.h"
+
+
 Axis::Axis()
 {
 
 };
 
-void Axis::Ainit(uint8_t _id_,uint8_t _endstop_pin_, uint8_t _encoder_pin_)
+void Axis::Ainit(int _id_, int _encp_, int _endp_)
 {
     _id = _id_;
-    _encoder_pin = _encoder_pin_;
-    _endstop_pin = _endstop_pin_;
+    _encp = _encp_;
+    _endp = _endp_;
 
-    attachInterrupt(digitalPinToInterrupt(_encoder_pin_), encoder_counter, RISING);
-    
-};
-
-uint8_t Axis::encoder_counter()
-{
-    _encoder_value++;
-    return _encoder_value;
-};
-
-bool Axis::endstop_check()
-{
-    return bool(digitalRead(_encoder_pin));
-};
-
-void Axis::reload()
-{
-    _encoder_value = 0;
-};
+    pinMode(_encp, INPUT_PULLUP);
+}
--- firmware/Kvantoriada_MEGA/Libs/Axis/Axis.h
+++ firmware/Kvantoriada_MEGA/Libs/Axis/Axis.h
@@ -1,29 +1,22 @@
-#ifndef AXIS_H
-#define AXIS_H
+#ifndef Axis_h
+#define Axis_h
 #include <Arduino.h>
 
 
-    class Axis
-    {
-    private:
-        uint8_t _id = 0;
-        uint8_t _endstop_pin = 0;
-        uint8_t _encoder_pin = 0;
-       
-        bool _endstop_mode = false;
-        bool _endstop_status = false;
-        uint8_t _encoder_value =0;
-    public:
-        Axis();
-        void Ainit(uint8_t _id_,uint8_t _endstop_pin_, uint8_t _encoder_pin_);
-        bool endstop_check();
-        uint8_t encoder_counter();
-        void reload();
-    
-    };
-    #define NORMAL false
-    #define INTERR true
-    
-    
+class Axis
+{
+private:
+    int _id = 0;
+    int _encp = 0;
+    int _endp = 0;
+    unsigned int encoder_value = 0;
 
+public:
+
+    Axis();
+    ~Axis();
+    void Ainit(int _id_, int _encp_, int _endp_);
+    boolean endstop_check();
+    unsigned int encoder_counter();
+};
 #endif
